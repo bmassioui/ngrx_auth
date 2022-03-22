f09a17337da5332cd5b9fd96d752dbedf91afdb8
bmassioui <bouchaib.massioui@gmail.com>
2022-03-22 12:30:14 +0000

    build login component

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,17 +2,23 @@ import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { ReactiveFormsModule } from "@angular/forms";
 import { RouterModule, Routes } from "@angular/router";
+
+import { LoginComponent } from "./components/login/login.component";
 import { RegisterComponent } from "./components/register/register.component";
 
 const authRoutes : Routes = [
     {
         path: 'register',
         component: RegisterComponent
+    },
+    {
+        path: 'login',
+        component: LoginComponent
     }
 ]
 
 @NgModule({
     imports: [CommonModule, RouterModule.forChild(authRoutes), ReactiveFormsModule],
-    declarations: [RegisterComponent]
+    declarations: [RegisterComponent, LoginComponent]
 })
 export class AuthModule{}
