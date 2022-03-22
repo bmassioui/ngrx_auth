4f117ff8c4cdbe3adcf4d761e162c698cd7ef1e4
bmassioui <bouchaib.massioui@gmail.com>
2022-03-22 12:48:56 +0000

    using formbuilder for register component

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,4 +1,5 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormControl, FormGroup, Validators } from "@angular/forms";
 
 @Component({
     selector: 'auth-register-component',
@@ -6,4 +7,30 @@ import { Component } from "@angular/core";
     styleUrls: ['./register.component.css']
 })
 
-export class RegisterComponent{}
+export class RegisterComponent implements OnInit{
+    public registerForm: FormGroup = new FormGroup({})
+
+    constructor(private formBuilder: FormBuilder) {}
+
+    ngOnInit(): void {
+        this.initializeRegisterForm();
+    }
+    
+    /**
+     * Initialize Register Form
+     */
+    initializeRegisterForm(): void{
+        this.registerForm = this.formBuilder.group({
+            username: new FormControl('', [Validators.required, Validators.minLength(7), Validators.pattern('^[a-zA-Z0-9]')]),
+            email: new FormControl('', [Validators.required,Validators.email]),
+            password: new FormControl('', [Validators.required, Validators.minLength(8), Validators.pattern('^(?=.*[A-Z].*[A-Z])(?=.*[!@#$&*])(?=.*[0-9].*[0-9])(?=.*[a-z].*[a-z].*[a-z]).{8}$')])
+        })
+    }
+
+    /**
+     * Register
+     */
+    register(): void{
+        console.log(this.registerForm.value)
+    }
+}
